5ebdc099a2c4d591608008ba5639be8b0b414aa9
Devin <Devinhe.dev@gmail.com>
2025-09-25 12:08:57 -0400

    centered looks of questions screen

---
+++ lib/data/questions.dart
@@ -0,0 +1,52 @@
+import 'package:flutter_application_1/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion("What is 0+1",
+  [
+    "1"
+    "2"
+    "3"
+    "4"
+  ],
+  ),
+  QuizQuestion("What is 1+1",
+  [
+    "2"
+    "3"
+    "4"
+    "5"
+  ],
+  ),
+  QuizQuestion("What is 2+1",
+  [
+    "3"
+    "4"
+    "5"
+    "6"
+  ],
+  ),
+  QuizQuestion("What is 3+1",
+  [
+    "4"
+    "5"
+    "6"
+    "7"
+  ],
+  ),
+    QuizQuestion("What is 4+1",
+  [
+    "5"
+    "6"
+    "7"
+    "8"
+  ],
+  ),
+    QuizQuestion("What is 5+1",
+  [
+    "6"
+    "7"
+    "8"
+    "9"
+  ],
+  ),
+];
